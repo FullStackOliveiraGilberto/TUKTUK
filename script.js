d5052768ce369a859cf7e5f0991d50a1f621a0e8
Gilberto Oliveira <skwcontadores@gmail.com>
2021-01-12 16:33:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,254 @@
+
+var translationsEN = {
+  HEADLINE: 'What an awesome module!',
+  PARAGRAPH: 'Srsly!',
+  PASSED_AS_ATTRIBUTE: 'I\'m passed as attribute value, cool ha?',
+  PASSED_AS_TEXT: 'Hey there! I\'m passed as text value!',
+  PASSED_AS_INTERPOLATION: 'Beginners! I\'m interpolated!',
+  TEXTOPASSADO: 'I can leraning ingles!', 
+ 
+  
+  SPEEDS_UP_DEVELOPMENT1: 'Speeds up development',
+  SPEEDS_UP_DEVELOPMENT_TEXT1: 'We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.',
+
+  SPEEDS_UP_DEVELOPMENT2: 'User Experience Focused',
+  SPEEDS_UP_DEVELOPMENT_TEXT2: 'By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.',
+  
+  
+  SPEEDS_UP_DEVELOPMENT3: 'Easy to work with',
+  SPEEDS_UP_DEVELOPMENT_TEXT3: 'We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.',
+
+
+  MODERNE_RESPONSIVE_TEXT4: 'A modern responsive front-end framework based on Material Design',
+  
+  MODERNE_RESPONSIVE_TEXT41: 'A modern responsive front-end framework based on Material Design',
+  
+  
+  CONTACT_US_TEXT5: 'THe tuktuk',
+  CONTACT_US_DESC_TEXT5: 'He is one of the millions of drivers in India who operate autorickshaws, known in much of Asia as tuk-tuks after the loud noise that a two-stroke engine makes while coughing a suffocating blue smoke trail. But Yadavs machine is different - it is electric, quiet and clean. There is no smoke and no cacophony when it trundles through the crowded streets of Gurugram, an urban conglomerate south of Indias capital, New Delhi, which has been classified as the most polluted city in the world. Yadav is part of a small, but growing, band of tuk-tuk electric drivers here who use a surprisingly convenient subscription service for interchangeable batteries that could change the way we view the practicality of electric vehicles (EVs). With the help of data and cloud computing, he is facing longstanding reservations about recharge times and reach limits.', 
+
+  COMPANY_DESC_TITLE: 'Company BIO',  
+  COMPANY_DESC_DESC:  'We are a team of university students working on this project as if it were our full-time job. Any suggestion send us an email.',
+  
+  TITULO_IMG1_PRINCIPAL: 'Request a Car / Vehicle',
+  TITULO_IMG2_PRINCIPAL: 'Car / Vehicle',
+ 
+
+  // menu
+  CADASTRO: 'Register', 
+  PERCURSOPAG: 'Route',
+  HISTORICOPAG: 'Historic',
+  HISTORICO_VIAGENS: 'Travel History',
+  SERVICOS: 'Services',
+  PERCURSO: 'Route',
+  SOLICITAR: 'REQUEST',
+  BTN_SOLICITAR: 'CALL A CAR',
+  BTN_MENSAGEM : 'MESSAGE', 
+  BTN_CANCELAR: 'CANCEL',
+  
+  LBL_TIME_WAIT: 'Time wait',
+  
+  CHIP_COR:'Color:',
+  CHIP_PLACA:'Vehicle plate: ',
+  CHIP_N_OCUPANTES:'Vehicle to: ', 
+  CHIP_CATEGORIA:'Category:&nbsp; ',
+  CHIP_ANO:'Year: &nbsp;',
+  CHIP_PONTUACAO:'Punctuation: &nbsp;',
+  CHIP_KM: 'Kilometer &nbsp;', 
+  IDIOMA: 'en',
+  INICIO: 'Start',
+ 
+ 
+   // titulo
+  TITLEPAG: 'Register',
+  
+  SUBTITULO2: 'Car / Vehicle',
+
+  VARIABLE_REPLACEMENT: 'Hi {{name}}',
+  MISSING_TRANSLATION: 'Oops! I have not been translated into Inglês...',
+  BUTTON_LANG_BR: 'Português',
+  BUTTON_LANG_DE: 'Deutsch',
+  BUTTON_LANG_EN: 'English'
+};
+ 
+var translationsDE= {
+  HEADLINE: 'Was für ein großartiges Modul!',
+  PARAGRAPH: 'Ernsthaft!',
+  PASSED_AS_TEXT: 'Hey! Ich wurde als text übergeben!',
+  PASSED_AS_ATTRIBUTE: 'Ich wurde als Attribut übergeben, cool oder?',
+  PASSED_AS_INTERPOLATION: 'Anfänger! Ich bin interpoliert!',
+  TEXTOPASSADO: 'Ich kann mich freuhren!', 
+  
+  SPEEDS_UP_DEVELOPMENT1: 'Beschleunigt die Entwicklung',
+  SPEEDS_UP_DEVELOPMENT_TEXT1: 'Wir haben die meisten der schweren Heben für Sie, um eine Standard-Stylings, die unsere benutzerdefinierten Komponenten enthalten. Darüber hinaus verfeinerten wir Animationen und Übergänge, um eine glattere Erfahrung für Entwickler zu bieten.',
+  SPEEDS_UP_DEVELOPMENT2: 'Beschleunigt die Entwicklun',
+  SPEEDS_UP_DEVELOPMENT_TEXT2: 'Wir haben die meisten der schweren Heben für Sie, um eine Standard-Stylings, die unsere benutzerdefinierten Komponenten enthalten. Darüber hinaus verfeinerten wir Animationen und Übergänge, um eine glattere Erfahrung für Entwickler zu bieten.',
+  SPEEDS_UP_DEVELOPMENT3: 'Beschleunigt die Entwicklun',
+  SPEEDS_UP_DEVELOPMENT_TEXT3: 'Wir haben die meisten der schweren Heben für Sie, um eine Standard-Stylings, die unsere benutzerdefinierten Komponenten enthalten. Darüber hinaus verfeinerten wir Animationen und Übergänge, um eine glattere Erfahrung für Entwickler zu bieten.',
+   
+  MODERNE_RESPONSIVE_TEXT4: 'Ein modernes responsives Front-End-Framework basierend auf Material Design',
+  
+ 
+  MODERNE_RESPONSIVE_TEXT41: 'Ein modernes responsives Front-End-Framework basierend auf Material Design',	
+  
+  CONTACT_US_TEXT5: 'o Tuktuk',
+  CONTACT_US_DESC_TEXT5: 'Er ist einer der Millionen Fahrer in Indien, die Autorikschas betreiben, die in weiten Teilen Asiens nach dem lauten Geräusch eines Zweitaktmotors beim Husten einer erstickenden blauen Rauchfahne als Tuk-Tuks bekannt sind. Aber Yadavs Maschine ist anders - sie ist elektrisch, leise und sauber. Es gibt keinen Rauch und keine Kakophonie, wenn es durch die überfüllten Straßen von Gurugram läuft, einem städtischen Konglomerat südlich der indischen Hauptstadt Neu-Delhi, das als die am stärksten verschmutzte Stadt der Welt eingestuft wurde. Yadav ist Teil einer kleinen, aber wachsenden Gruppe von Tuk-Tuk-Elektrofahrern, die einen überraschend bequemen Abonnementdienst für austauschbare Batterien nutzen, der unsere Sicht auf die Praktikabilität von Elektrofahrzeugen verändern könnte. Mit Hilfe von Daten- und Cloud-Computing sieht er sich langjährigen Vorbehalten gegenüber Ladezeiten und Grenzüberschreitungen gegenüber. ;',
+ 
+   COMPANY_DESC_TITLE:'Company BIO',
+  
+  COMPANY_DESC_DESC: 'Wir sind ein Team von Studenten, die an diesem Projekt arbeiten, als wäre es unser Vollzeitjob. Jeder Vorschlag senden Sie uns eine E-Mail.',
+  
+  TITULO_IMG1_PRINCIPAL: 'Auto / Fahrzeug',
+   TITULO_IMG2_PRINCIPAL: 'Fordern Sie ein Auto / Fahrzeug an',
+  
+
+ 
+ 
+// menu
+  CADASTRO: 'Registrieren', 
+  PERCURSOPAG:'JOURNEY',
+  HISTORICOPAG: 'Geschichte',
+  HISTORICO_VIAGENS: 'Reise Geschichte',
+  SERVICOS: 'Dienstleistungen',
+  PERCURSO: 'Journey',
+  SOLICITAR: 'REQUEST',
+  BTN_SOLICITAR: 'CALL-Auto',
+  BTN_MENSAGEM : 'NACHRICHT', 
+  BTN_CANCELAR: 'STORNIEREN',
+  
+  LBL_TIME_WAIT: 'Standby-Zeit',
+    
+  CHIP_COR:'Farbe: ',
+  CHIP_PLACA:'Teure Board: ',
+  CHIP_N_OCUPANTES:'Anzahl der insassen: ', 
+  CHIP_CATEGORIA:'Kategorie: ',
+  CHIP_ANO:'Jahr: ',
+  CHIP_PONTUACAO:'Zeichensetzung: ',
+  CHIP_KM: 'Kilometer',
+  IDIOMA: 'de',
+  INICIO: 'Zuerst',
+  
+ 
+
+ // titulo
+  TITLEPAG: 'Registrieren', 
+  
+  SUBTITULO2: 'Auto / Fahrzeug',
+    
+  VARIABLE_REPLACEMENT: 'Hi {{name}}',
+  //MISSING_TRANSLATION is ... missing :)
+  BUTTON_LANG_BR: 'Português',
+  BUTTON_LANG_DE: 'Deutsch',
+  BUTTON_LANG_EN: 'Englisch'
+};
+
+
+var translationsBR = {
+  HEADLINE: 'O que um módulo incrível!!',
+  PARAGRAPH: 'Srsly!',
+  PASSED_AS_TEXT: 'Olá! Eu sou passado como o valor do texto!',
+  PASSED_AS_ATTRIBUTE: 'Eu sou passado como o valor do atributo, ha fresco?',
+  PASSED_AS_INTERPOLATION: 'Iniciantes! Estou interpolado',
+  TEXTOPASSADO: 'Eu posso ler português!', 
+  
+  SPEEDS_UP_DEVELOPMENT1: 'Desenvolvimento Rápido',
+  SPEEDS_UP_DEVELOPMENT_TEXT1: 'Fizemos a maior parte do trabalho pesado para você fornecer um estilo padrão que incorpora nossos componentes personalizados. Além disso, refinamos animações e transições para oferecer uma experiência mais suave aos desenvolvedores.',
+
+  SPEEDS_UP_DEVELOPMENT2: 'Experiência do usuário focada',
+  SPEEDS_UP_DEVELOPMENT_TEXT2: 'Utilizando elementos e princípios do Material Design, fomos capazes de criar uma estrutura que incorpora componentes e animações que fornecem mais feedback aos usuários. Além disso, um único sistema de resposta subjacente em todas as plataformas permite uma experiência de usuário mais unificada.',
+  
+  
+  SPEEDS_UP_DEVELOPMENT3: 'Easy to work with',
+  SPEEDS_UP_DEVELOPMENT_TEXT3: 'Fornecemos documentação detalhada, bem como exemplos de código específicos para ajudar os novos usuários a começar. Também estamos sempre abertos ao feedback e podemos responder a quaisquer perguntas que um usuário possa ter sobre Materialise.',
+
+  MODERNE_RESPONSIVE_TEXT4: 'Uma estrutura de front-end responsiva moderna baseada em Material Design',
+
+  MODERNE_RESPONSIVE_TEXT41: 'Uma estrutura de front-end responsiva e moderna baseada em Material Design',	
+  	
+  CONTACT_US_TEXT5: 'O tuktuk',	
+  CONTACT_US_DESC_TEXT5: 'Ele é um dos milhões de motoristas na Índia que operam autorickshaws, conhecidos em grande parte da Ásia como tuk-tuks após o barulho alto que um motor de dois tempos faz enquanto tosse um rastro de fumaça azul sufocante. Mas a máquina de Yadav é diferente – é elétrica, silenciosa e limpa. Não há fumaça e nem cacofonia quando ele trundles através de ruas lotadas de Gurugram, um conglomerado urbano ao sul da capital da Índia, Nova Deli, que foi classificado como a cidade mais poluída do mundo. Yadav faz parte de uma pequena, mas crescente, banda de motoristas elétricos tuk-tuk aqui que usam um serviço de assinatura surpreendentemente conveniente para baterias permutáveis que poderiam mudar a forma como consideramos a praticidade dos veículos elétricos (EVs). Com a ajuda de dados e computação em nuvem, ele está enfrentando reservas de longa data sobre tempos de recarga e limites de alcance.',
+  
+  COMPANY_DESC_TITLE:'Company BIO',
+  COMPANY_DESC_DESC: 'Somos uma equipe de estudantes universitários trabalhando neste projeto como se fosse nosso trabalho em tempo integral. Qualquer sugestão envie-nos um email.',
+  
+  TITULO_IMG1_PRINCIPAL: 'Carro/Veículo',
+  TITULO_IMG2_PRINCIPAL: 'Solicite um Carro/Veículo',  
+  
+  
+  // menu
+  CADASTRO: 'Cadastro',
+  PERCURSOPAG:'Percurso',  
+  HISTORICOPAG: 'Histórico',
+  HISTORICO_VIAGENS: 'Histórico de Viagens',
+  SERVICOS: 'Serviços',
+  PERCURSO: 'Percuso',
+  SOLICITAR: 'Solicitar',
+  IDIOMA: 'br',
+  INICIO: 'Início',
+  BTN_SOLICITAR:'SOLICITAR',
+  BTN_MENSAGEM: 'MENSAGEM', 
+  BTN_CANCELAR: 'CANCELAR',
+  
+  LBL_TIME_WAIT: 'Tempo de Espera: ',
+  
+  
+  CHIP_COR:'Cor: ',
+  CHIP_PLACA:'Placa: ',
+  CHIP_N_OCUPANTES:'N. Ocupantes: ', 
+  CHIP_CATEGORIA:'Categoria: ',
+  CHIP_ANO:'Ano:    ',
+  CHIP_PONTUACAO:'Pontuação: ',
+  CHIP_KM:'Kilometros: ',
+  
+  // titulo
+  TITLEPAG: 'Cadastro', 
+ SUBTITULO2: 'Carro/Veículo',
+ 
+ 
+  VARIABLE_REPLACEMENT: 'Oi {{name}}',
+  MISSING_TRANSLATION: 'Oops! Erro na tradução para o português...',
+  BUTTON_LANG_BR: 'Português',
+  BUTTON_LANG_DE: 'Alemão',
+  BUTTON_LANG_EN: 'Ingles'
+  
+};
+
+
+ 
+var app = angular.module('myApp', ['pascalprecht.translate']);
+ 
+app.config(['$translateProvider', function ($translateProvider) {
+  // add translation tables
+  
+  $translateProvider.translations('br', translationsBR);
+  $translateProvider.translations('en', translationsEN);
+  $translateProvider.translations('de', translationsDE);
+  
+  lang_storage = localStorage.getItem("ls_language");
+  
+  if (lang_storage != '' ) {
+    $translateProvider.preferredLanguage(lang_storage);
+  } else {
+    $translateProvider.preferredLanguage('en');
+  }
+  
+  $translateProvider.fallbackLanguage('br');
+}]);
+ 
+app.controller('Ctrl', ['$translate', '$scope', function ($translate, $scope) {
+   $scope.changeLanguage = function (langKey) {
+   var vlanguage = localStorage.setItem("ls_language", langKey);
+   $translate.use(langKey);
+  };
+    
+  $scope.getLanguage2 = function (langKey) {
+      document.getElementById("test").innerHTML = localStorage.getItem("ls_language");
+  };
+}]); /*Fim controller Ctrl*/
+
+
+
+
+ 
+
+
